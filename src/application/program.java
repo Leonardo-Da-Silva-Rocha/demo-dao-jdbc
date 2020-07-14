28d24c1ee8b592c8be062704e2ac5d4db9693881
Leonardo-Da-Silva-rocha <leosilvarocha.lsr@gmail.com>
2020-07-13 22:22:40 -0300

    Departamen entity class

---
+++ src/application/program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departamento;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1,"books");
+		System.out.println(obj);
+		
+
+	}
+
+}
